6441ee187782ec6c091074e2e2bd858b7a5a76d2
klisit <klisit9527@outlook.com>
2021-05-05 17:15:05 +0800

    chore(deps): 加入 postcss 添加自动添加前缀

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Description: string
+ * @Author: klisit
+ * @Date: 2021-04-18 10:28:21
+ * @LastEditTime: 2021-04-18 23:15:10
+ * @LastEditors: klisit
+ */
+module.exports = {
+  // Add you postcss configuration here
+  // Learn more about it at https://github.com/webpack-contrib/postcss-loader#config-files
+  plugins: {
+    autoprefixer: {},
+  },
+}
