9b98db88c1ca1d51516a570457d8c23f0e67b542
klisit <klisit9527@outlook.com>
2021-05-10 22:47:30 +0800

    build(styles): 添加 normalize.css 重置样式

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
+import 'normalize.css/normalize.css'
 
 createApp(App).use(store).use(router).mount('#app')
